ebf9bf78ad8b7d1d0f92e2f94e88104e0276a537
IT1811002 <IT1811002@LAPTOP-8K528EU6>
2019-09-08 16:47:36 +0900

    1

---
+++ rest_M2/src/main/java/com/M2/Biz/RestM2Application.java
@@ -0,0 +1,13 @@
+package com.M2.Biz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestM2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestM2Application.class, args);
+	}
+
+}
---
+++ rest_M2/src/test/java/com/M2/Biz/RestM2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.M2.Biz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestM2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
